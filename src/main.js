7c6870389a145f46bfc3276f49c88ae9101f3c1d
Hoang Nam <namhoang.td09@gmail.com>
2016-03-22 09:20:19 +0700

    add ESLint

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // Expression bodies
+
 class View {
     constructor(name) {
         this._name = name;
@@ -32,26 +33,3 @@ var bob = {
   }
 }
 
-class SkinnedMesh extends THREE.Mesh {
-  constructor(geometry, materials) {
-    super(geometry, materials);
-
-    this.idMatrix = SkinnedMesh.defaultMatrix();
-    this.bones = [];
-    this.boneMatrices = [];
-    //...
-  }
-  update(camera) {
-    //...
-    super.update();
-  }
-  get boneCount() {
-    return this.bones.length;
-  }
-  set matrixType(matrixType) {
-    this.idMatrix = SkinnedMesh[matrixType]();
-  }
-  static defaultMatrix() {
-    return new THREE.Matrix4();
-  }
-}
